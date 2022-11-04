b97888b2ba7c9cae2717445e5d4fcdeb0a12996b
mitsu <d.mitsuoka@newbees.jp>
2022-11-04 09:52:43 +0900

    firstcommit

--- src/batch/db/refresh.go
+++ src/batch/db/refresh.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Asia%sTokyo", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_DATABASE"), "%2F")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	
 	if err != nil {
 		panic(err.Error)
 	}
